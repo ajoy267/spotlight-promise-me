5b5378fe73e55e74d58019526f20e30d1ef6e342
ajoy267 <andrew.joy12@hotmail.com>
2022-01-25 11:54:54 -0800

    working on api calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { useEffect } from 'react';
+import {
+  fetchBreweries1,
+  fetchBreweries2,
+  fetchCatFacts,
+  fetchPinBall,
+} from './services/promise-me';
+
 export default function App() {
+  useEffect(() => {
+    fetchBreweries1();
+    fetchBreweries2();
+    fetchPinBall();
+    fetchCatFacts();
+  });
   return <h1>Hello World</h1>;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,14 +1,38 @@
 /**
- * TODO:
+ * TODO: Write a fetch request to an API using .then()
  */
+export function fetchBreweries1() {
+  const resp = fetch('https://api.openbrewerydb.org/breweries')
+    .then((data) => data.json())
+    .then((brewData) => console.log(brewData));
+  return resp;
+}
+
+export async function fetchBreweries2() {
+  const resp = await fetch('https://api.openbrewerydb.org/breweries');
+  const data = await resp.json();
+  console.log(data);
+  return data;
+}
 
 /**
  * TODO:
  */
-
+export function fetchPinBall() {
+  const resp = fetch('https://ponball.herokuapp.com/api/v1/machines')
+    .then((data) => data.json())
+    .then((pinBallData) => console.log(pinBallData));
+  return resp;
+}
 /**
  * TODO:
  */
+export function fetchCatFacts() {
+  const resp = fetch('https://cat-fact.herokuapp.com')
+    .then((data) => data.json())
+    .then((catData) => console.log(catData));
+  return resp;
+}
 
 /**
  * TODO:
